1f2b252f59392123b75aada19a49bbc7b203e8ed
Jacek Gach <projekt@ceti.com.pl>
2023-03-13 16:04:24 +0100

    kolejny6

--- kopia/2/indexe.php
+++ kopia/2/indexe.php
@@ -23,18 +23,16 @@
 			<input type="button" onclick="set7ToInput()" name="button7" id="button7" value="7" style="margin-right: 10px">
 			<input type="button" onclick="set8ToInput()" name="button8" id="button8" value="8" style="margin-right: 10px">
 			<input type="button" onclick="set9ToInput()" name="button9" id="button9" value="9" style="margin-right: 10px">
-			<input type="button" onclick="setIlorToInput()" name="button13" value="/" style="margin-right: 10px">
-			<input type="button" onclick="setClearEToInput()" name="button15" value="CE"><br><br>
+			<input type="button" onclick="setIlorToInput()" name="button13" value="/"><br><br>
 			<input type="button" onclick="set4ToInput()" name="button4" id="button4" value="4"; style="margin-right: 10px">
 			<input type="button" onclick="set5ToInput()" name="button5" id="button5" value="5"; style="margin-right: 10px">
 			<input type="button" onclick="set6ToInput()" name="button6" id="button6" value="6"; style="margin-right: 10px">
-			<input type="button" onclick="setIlocToInput()" name="button12" value="*" style="margin-right: 10px">
-			<input type="button" onclick="setClearToInput()" name="button16"value="C"><br><br>
+			<input type="button" onclick="setIlocToInput()" name="button12" value="*"><br><br>
 			<input type="button" onclick="set1ToInput()" name="button1" id="button1" value="1"; style="margin-right: 10px">
 			<input type="button" onclick="set2ToInput()" name="button2" id="button2" value="2"; style="margin-right: 10px">
 			<input type="button" onclick="set3ToInput()" name="button3" id="button3" value="3"; style="margin-right: 10px">
 			<input type="button" onclick="setRozToInput()" name="button11" value="-"><br>
-			<input type="button" onclick="setWynToInput()" name="button14" value="=" style="width: 20px; height: 62px; position: fixed; top: 140px; left: 150px; margin-right: 10px"><br>
+			<input type="button" onclick="setWynToInput()" name="button14" value="=" style="width: 20px; height: 62px; position: fixed; top: 139px; left: 140px; margin-right: 10px"><br>
 			<input type="button" onclick="set0ToInput()" name="button0" id="button0" value="0"; style="width: 60px; margin-right: 10px">
 			<input type="button" onclick="setcToInput()" name="buttonc" id="buttonc" value=","; style="width: 22px; margin-right: 10px">
 			<input type="button" onclick="setSumToInput()" name="button10" value="+" style="width: 20px">
@@ -85,22 +83,6 @@
 		
 		function set9ToInput() {
         document.getElementById("no1").value = document.getElementById("no1").value + "9";
-		}
-		
-		function setClearEToInput() {
-		document.getElementById("no1").value = ""
-		}
-		
-		function setClearToInput() {
-		document.getElementById("no1").value = "2"
-		document.getElementById("no2").value = "3"
-		znval1=0;
-		znval2=0;
-		znval3=0;
-		znval4=0;
-		val3=0;
-		wynik1=0;
-		val2=0;
 		}
 		
 		
